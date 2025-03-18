48e07e1aa17ddb25957607219caa4e42f41a1b9c
Ashish Chourasiya <ashishchourasiya2828@gmai.com>
2025-03-18 05:45:51 +0530

    Refactor pattern printing in FirstClass and add new questions class with pattern logic

---
+++ Questions/questions.java
@@ -0,0 +1,17 @@
+
+class questions {
+    public static void main(String args[]) {
+        // Pattern 1
+        for (int i=1; i<=5;i++){
+            for (int j = 1; j <=4; j++) {
+                if(i==1 || j==1 || i==5 || j==4){
+                    System.out.print(" *");
+                }                
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
