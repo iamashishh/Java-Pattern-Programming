c191d812e3f5d1e5e3bef8c2d8e2dc115fb23138
Ashish Chourasiya <ashishchourasiya2828@gmai.com>
2025-03-18 05:57:21 +0530

    Add user input for pattern size and clean up commented code in questions.java

--- Questions/questions.java
+++ Questions/questions.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class questions {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
         // Pattern 1
         // for (int i=1; i<=5;i++){
         //     for (int j = 1; j <=4; j++) {
@@ -17,17 +19,27 @@ class questions {
     
         // Pattern 2
 
-        Scanner sc = new Scanner(System.in);
         
+        // int n= sc.nextInt();
+
+        // for(int i=1; i<=n;i++){
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(" *");
+                
+        //     }
+        //     System.out.println();
+        // }
+
+        // Pattern 3
+
         int n= sc.nextInt();
 
-        for(int i=1; i<=n;i++){
+        for (int i = n; i>=1; i--) {
             for (int j = 1; j <=i; j++) {
-                System.out.print(" *");
-                
+                System.out.print(" *");               
             }
             System.out.println();
+            
         }
-
     }
 }
